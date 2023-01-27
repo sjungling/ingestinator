93d5d5b1aaf0a7f364d54b11352d926e67d45123
Scott Jungling <scott@moderne.io>
2023-01-26 20:47:28 -0800

    chore: docker…why

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,16 @@
 # Container image that runs your code
-FROM ubuntu:20.04
+FROM alpine:edge
 
-RUN apt-get update
-RUN yes | apt-get install zip curl wget unzip zip
+ENV SDKMAN_DIR=/root/.sdkman
+RUN apk --update add libc6-compat bash curl git zip unzip wget ca-certificates && \
+    curl -s "https://get.sdkman.io" | bash && \
+    echo "sdkman_auto_answer=true" > $SDKMAN_DIR/etc/config && \
+    echo "sdkman_auto_selfupdate=false" >> $SDKMAN_DIR/etc/config
 RUN rm /bin/sh && ln -s /bin/bash /bin/sh
-
-RUN curl -s "https://get.sdkman.io" | bash
-
 # Copies your code file from your action repository to the filesystem path `/` of the container
 COPY entrypoint.sh /entrypoint.sh
+# make entrypoint.sh executable
+RUN chmod +x /entrypoint.sh
 
 # Code file to execute when the docker container starts up (`entrypoint.sh`)
-ENTRYPOINT [ "./entrypoint.sh" ]
+ENTRYPOINT  [ "/usr/bin/env", "bash", "-c", "/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,35 +1,36 @@
-#!/bin/sh -l
+#!/bin/bash
+source $SDKMAN_DIR/bin/sdkman-init.sh
 
-echo "Hello $1"
-time=$(date)
-echo "time=$time" >> $GITHUB_OUTPUT
-# #!/bin/bash
-# source "$HOME/.sdkman/bin/sdkman-init.sh"
+echo "Lets get ingesting!"
+echo "Using the following settings:"
+echo "REPOSITORY_TYPE: ${REPOSITORY_TYPE}"
+echo "REPOSITORY_URL: ${REPOSITORY_URL}"
+echo "REPOSITORY_USERNAME: ${REPOSITORY_USERNAME}"
+echo "REPOSITORY_PASSWORD: ${REPOSITORY_PASSWORD}"
 
-# echo "Lets get ingesting!"
-# echo "Using the following settings:"
-# echo "REPOSITORY_TYPE: ${REPOSITORY_TYPE}"
-# echo "REPOSITORY_URL: ${REPOSITORY_URL}"
-# echo "REPOSITORY_USERNAME: ${REPOSITORY_USERNAME}"
-# echo "REPOSITORY_PASSWORD: ${REPOSITORY_PASSWORD}"
+echo "JAVA_VERSION: ${JAVA_VERSION}"
+echo "BUILD_ACTION: ${BUILD_ACTION}"
+echo "BUILD_TOOL: ${BUILD_TOOL}"
+echo "STYLE: ${STYLE}"
 
-# echo "JAVA_VERSION: ${JAVA_VERSION}"
-# echo "BUILD_ACTION: ${BUILD_ACTION}"
-# echo "BUILD_TOOL: ${BUILD_TOOL}"
-# echo "STYLE: ${STYLE}"
+sdk update
+# if java version is 8; then download java 8 otherwise get java 11
+if [ "$JAVA_VERSION" = "8" ]; then
+    echo "Installing java 8.0.362-zulu"
+    sdk install java 8.0.362-zulu as default
+elif [ "$JAVA_VERSION" = "11" ]; then
+    echo "Installing java 11.0.11.hs-adpt"
+    sdk install java 11.0.11.hs-adpt as default
+else 
+    echo "Installing java 17.0.6-tem "
+    sdk install java 17.0.6-tem  as default
+fi  
 
-# sdk update
-# # if java version is 8; then download java 8 otherwise get java 11
-# if [ "$JAVA_VERSION" = "8" ]; then
-#     sdk install java 8.0.362-zulu
-# elif [ "$JAVA_VERSION" = "11" ]; then
-#     sdk install java 11.0.11.hs-adpt
-# else 
-#     sdk install java 17.0.6-tem
-# fi
-
-
-# # if BUILD_TOOL is gradle then download gradle otherwise download maven
+echo "Current PATH: $PATH"
+echo "Current JAVA_HOME: $JAVA_HOME"
+which java
+$(which java) -version
+# if BUILD_TOOL is gradle then download gradle otherwise download maven
 # if [ "$BUILD_TOOL" == "gradle" ]; then
 #     # if STYLE is not null; then set the command arguments otherwise get the other command arguments
 #     if [ "$STYLE" != "" ]; then
@@ -39,16 +40,16 @@ echo "time=$time" >> $GITHUB_OUTPUT
 #     fi
 #     # if REPOSITORY_TYPE is nexus; then download init script otherwise get the other init script
 #     if [ "$REPOSITORY_TYPE" == "nexus" ]; then
-#         curl -o init.gradle https://raw.githubusercontent.com/moderneinc/enterprise-jenkins-ingest/main/gradle/init-nexus.gradle
+#         curl -sS -o init.gradle https://raw.githubusercontent.com/moderneinc/enterprise-jenkins-ingest/main/gradle/init-nexus.gradle
 #     else
-#         curl -o init.sh https://raw.githubusercontent.com/philips-software/docker-ci-scripts/master/init.sh
+#         curl -sS -o init.sh https://raw.githubusercontent.com/philips-software/docker-ci-scripts/master/init.sh
 #     fi
 
 #     # run ./gradle $GRADLE_ARGS moderneJar conditionally with artifactoryPublish if REPOSITORY_TYPE is artifactory
 #     if [ "$REPOSITORY_TYPE" = "artifactory" ]; then
-#         echo "./gradlew $GRADLE_ARGS moderneJar artifactoryPublish"
+#         ./gradlew $GRADLE_ARGS moderneJar artifactoryPublish
 #     else
-#         echo "./gradlew $GRADLE_ARGS moderneJar"
+#         ./gradlew $GRADLE_ARGS moderneJar
 #     fi    
 
 # # else if BUILD_TOOL starts with  "maven" or "mvnw"; then download maven
@@ -56,9 +57,11 @@ echo "time=$time" >> $GITHUB_OUTPUT
 
 #     # if BUILT_TOOL is maven or mvnw then download maven 3.8.6 for $JAVA_VERSION otherwise download maven 3.3.9 JDK 8
 #     if [ "$BUILD_TOOL" = "maven" ] || [ "$BUILD_TOOL" = "mvnw" ]; then
-#         sdk install maven 3.8.6
+#         echo "Installing maven 3.8.6"
+#         sdk install maven 3.8.6  &>/dev/null
 #     else
-#         sdk install maven 3.3.9
+#         echo "Installing maven 3.3.9"
+#         sdk install maven 3.3.9  &>/dev/null
 #     fi
 
 #     curl -o add-mvn-configuration.sh https://raw.githubusercontent.com/moderneinc/enterprise-jenkins-ingest/main/maven/add-mvn-configuration.sh
@@ -75,14 +78,18 @@ echo "time=$time" >> $GITHUB_OUTPUT
 
 #     # if BUILD_TOOL is mvwn; then run mvnw otherwise run mvn
 #     if [ "$BUILD_TOOL" = "mvnw" ]; then
-#         echo "./mvnw $MVN_ARGS"
+#         ./mvnw $MVN_ARGS
 #     else
-#         echo "./mvn $MVN_ARGS"
+#         ./mvn $MVN_ARGS
 #     fi
 
 
 # fi
 
-# sdk install groovy
-# curl -o publish-ast.groovy https://gitlab.com/moderneinc/moderne-gitlab-ingest/-/raw/main/publish-ast.groovy
+# echo "Installing groovy "
+# sdk install groovy  &>/dev/null
+# curl -sS -o publish-ast.groovy https://gitlab.com/moderneinc/moderne-gitlab-ingest/-/raw/main/publish-ast.groovy
+# export NEXUS_CREDENTIALS="${REPOSITORY_USERNAME}:${REPOSITORY_PASSWORD}"
+# export NEXUS_URL="${REPOSITORY_URL}"
+
 # echo "groovy publish-ast.groovy"
